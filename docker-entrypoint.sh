9c446f7622c21964afc35cabdbbb1c42afe5f253
root <root@node1.cluster.local>
2020-11-20 14:46:08 +0000

    without timeout

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-echo "server=$NETXMS_SERVER\nsessionTimeout=$SESSIONTIMEOUT\nenableAdvancedSettings=$ENABLEADVANCEDSETTINGS" >/usr/local/tomcat/lib/nxmc.properties
+#echo "server=$NETXMS_SERVER\nsessionTimeout=$SESSIONTIMEOUT\nenableAdvancedSettings=$ENABLEADVANCEDSETTINGS" >/usr/local/tomcat/lib/nxmc.properties
+echo "server=$NETXMS_SERVER\nenableAdvancedSettings=$ENABLEADVANCEDSETTINGS" >/usr/local/tomcat/lib/nxmc.properties
 
 catalina.sh run
